888df1a7b860abea51050ac2a583be3586dc0f4f
Dmitriy <dimukai@yandex.ru>
2022-03-27 22:22:36 +0400

    Home Work 2

---
+++ OOP_Home_Work_Bank/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace OOP_Home_Work_Bank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            Account account = new Account();
+
+            account.FillTypeAccount(TypeOfBankAccount.DEBIT);
+
+            account.FillNumber(random.Next(1, 99999999));
+
+            account.FillBalance(100000);
+
+            account.FullInfo();
+        }
+    }
+}
---
+++ OOP_Home_Work_Bank/TypeOfBankAccount.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Home_Work_Bank
+{
+    public enum TypeOfBankAccount
+    {
+        DEBIT,
+        CREDIT,
+        DEPOSIT,
+        BUDGET
+    }
+}
