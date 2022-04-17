e7ed8cc8f89148c32d11e2fc69a0167871214b00
Dmitriy <dimukai@yandex.ru>
2022-04-17 20:45:11 +0400

    не понимаю как 3 делать в 4 дз

---
+++ home_work_4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace home_work_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Building building = new();
+            building.Add(3, 4, 5);
+
+            Building building2 = new();
+            building2.Add(4, 8, 7, 5);
+
+            building.Print();
+            building2.Print();
+            building.Print();
+        }
+    }
+}
